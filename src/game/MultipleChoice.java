f9dff46460c0ecd70556c73c5407911ef3734ce7
Duy <duyvub1206@gmail.com>
2023-11-21 18:25:28 +0700

    multipleChoice game

---
+++ src/game/MultipleChoice.java
@@ -0,0 +1,138 @@
+package game;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+import game.question.Question;
+
+public class MultipleChoice extends GameManagement implements GameInterface {
+    private static final int MAX = 33482;
+    private String path = "resources/data/MultipleChoiceData.txt";
+    private List<Question> questionList = new ArrayList<>();
+
+    public MultipleChoice() {
+        point = 0;
+        health = 3;
+    }
+
+    private void insertQuestionFromFile() {
+        try {
+            FileReader fr = new FileReader(path);
+            BufferedReader bf = new BufferedReader(fr);
+            String line;
+
+            while ((line = bf.readLine()) != null) {
+                String[] s = line.split("\\s+");
+
+                String answers = s[0];
+
+                String question = "";
+                for (int i = 1; i < s.length; i++) {
+                    if (answers.equalsIgnoreCase(s[i])) {
+                        question += "___" + " ";
+                    } else {
+                        question += s[i] + " ";
+                    }
+                }
+                questionList.add(new Question(question, answers));
+            }
+
+            bf.close();
+            fr.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("không tìm thấy file");
+        } catch (IOException e) {
+            System.out.println("Lỗi đọc file");
+        } catch (Exception e) {
+            System.out.println("Lỗi khác với file");
+        }
+
+    }
+
+    private List<String> randomAnswers(String answer) {
+        List<String> list = new ArrayList<>();
+        Random rand = new Random();
+        list.add(answer);
+        while (list.size() < 5) {
+            int index = rand.nextInt(MAX);
+            list.add(questionList.get(index).getCorrectAnswers());
+        }
+
+        //đảo đáp án.
+        Collections.shuffle(list); 
+        return list;
+    }
+
+    private void printInfo() {
+        System.out.println("----------Your point:  " + this.point + " --------");
+        System.out.println("----------Your health: " + this.health + " --------" + "\n\n");
+    }
+
+    public void start() {
+        insertQuestionFromFile();
+        Scanner input = new Scanner(System.in);
+        int stt = 1;
+        System.out.println("-------------NEW GAME ------------");
+
+        while (true) {
+            Question x = this.randomQuestion();
+            x.setListAnswers(randomAnswers(x.getCorrectAnswers()));
+            x.printQuestion(stt);
+            stt++;
+
+            System.out.print("Your Answers: ");
+            char userAnswers = input.next().charAt(0);
+            userAnswers = Character.toUpperCase(userAnswers);
+            if (x.checkAnswers(userAnswers) == true) {
+                System.out.println("\n\n\n\nCORRECT !");
+                point += 1;
+            } else {
+                System.out.println("\n\n\n\nINCORRECT ! \t\t CORRECT ANSWERS: " + x.getCorrectAnswers());
+                health -= 1;
+            }
+            printInfo();
+            if (isEndGame() == true) {
+                if (point == 10)
+                    System.out.println("--------------YOU WIN !---------");
+                else
+                    System.out.println("--------------YOU LOSE !---------");
+                break;
+            }
+        }
+        //input.close();
+        playAgain();
+    }
+
+    private void playAgain() {
+        boolean check = true;
+        Scanner input = new Scanner(System.in);
+        while (check) {
+            System.out.println("Do you want to play again? [Y/n]\t");
+            char userInput = input.next().charAt(0);
+            if (userInput == 'Y' || userInput == 'y') {
+                point = 0;
+                health = 3;
+                start();
+            } else check = false;
+        }
+    }
+
+    private Question randomQuestion() {
+        Random rand = new Random();
+        int index = rand.nextInt(MAX);
+        questionList.get(index).toString();
+        return questionList.get(index);
+    }
+
+    private boolean isEndGame() {
+        return point == 10 || health == 0;
+    }
+}
---
+++ src/game/question/Question.java
@@ -0,0 +1,71 @@
+package game.question;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Question {
+    private String question;
+    private String correctAnswers;
+    private List<String> listAnswers = new ArrayList<>();
+
+    public Question() {
+
+    }
+
+    public Question(String question, String correctAnswers) {
+        this.question = question;
+        this.correctAnswers = correctAnswers;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getCorrectAnswers() {
+        return correctAnswers;
+    }
+
+    public void setCorrectAnswers(String correctAnswers) {
+        this.correctAnswers = correctAnswers;
+    }
+
+    public List<String> getListAnswers() {
+        return listAnswers;
+    }
+
+    public void setListAnswers(List<String> listAnswers) {
+        this.listAnswers = listAnswers;
+    }
+
+    
+    public boolean checkAnswers(String answer) {
+        if (correctAnswers.equalsIgnoreCase(answer)) {
+            return true;
+        } else return false;
+    }
+
+    public void printQuestion(int stt) {
+        String s = "Question " + Integer.toString(stt) + ": ";
+        s += question + "\n";
+        s += "A. " + listAnswers.get(0) + "\n"
+            + "B. " + listAnswers.get(1) + "\n"
+            + "C. " + listAnswers.get(2) + "\n"
+            + "D. " + listAnswers.get(3) + "\n";
+        System.out.println(s);
+    }
+
+    public boolean checkAnswers(char userAnswers) {
+        userAnswers = Character.toUpperCase(userAnswers);
+        int index = userAnswers - 'A';
+        String answers = listAnswers.get(index);
+        if (correctAnswers.equals(answers) == true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
