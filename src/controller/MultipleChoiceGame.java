a59424c9bb98e02ce58262775fc5859a2e7e3e4e
qduytran <139730440+qduytran@users.noreply.github.com>
2023-11-28 10:08:11 +0700

    complete multiple choice game

--- src/controller/MultipleChoiceGame.java
+++ src/controller/MultipleChoiceGame.java
@@ -16,9 +16,9 @@ public class MultipleChoiceGame extends GameController {
     @FXML
     TextArea contentQuestion;
     @FXML
-    Button selectAbtn, selectBbtn, selectCbtn, selectDbtn, nextquestionbtn;
+    Button selectAbtn, selectBbtn, selectCbtn, selectDbtn, nextquestionbtn, replayBtn;
     @FXML
-    Label correct, wrong;
+    Label correct, wrong, healthText, scoreText;
     @FXML
     ImageView correct1, wrong1;
 
@@ -31,21 +31,42 @@ public class MultipleChoiceGame extends GameController {
         game = new MultipleChoice();
         game.insertQuestionFromFile();
         displayQuestion();
+        replayBtn.setDisable(true);
         selectAbtn.setOnAction(event -> handleAnswer(0));
         selectBbtn.setOnAction(event -> handleAnswer(1));
         selectCbtn.setOnAction(event -> handleAnswer(2));
         selectDbtn.setOnAction(event -> handleAnswer(3));
         nextquestionbtn.setOnAction(event -> displayQuestion());
-
+        replayBtn.setOnAction(event -> handleReplay());
         contentQuestion.setWrapText(true);
         contentQuestion.setEditable(false);
         correct.setVisible(false);
         correct1.setVisible(false);
         wrong.setVisible(false);
         wrong1.setVisible(false);
+
+        healthText.setText("Health: " + game.getHealth());
+        scoreText.setText("Score: " + game.getPoint());
+
+    }
+
+    private void handleReplay() {
+        nextquestionbtn.setDisable(false);
+        replayBtn.setDisable(true);
+        game.setPoint(0);
+        game.setHealth(3);
+        displayQuestion();
     }
 
     private void displayQuestion() {
+        correct.setVisible(false);
+        correct1.setVisible(false);
+        wrong.setVisible(false);
+        wrong1.setVisible(false);
+        selectAbtn.setDisable(false);
+        selectBbtn.setDisable(false);
+        selectCbtn.setDisable(false);
+        selectDbtn.setDisable(false);
         if (game != null) {
             currentQuestion = game.randomQuestion();
             contentQuestion.setText(currentQuestion.getQuestion());
@@ -62,19 +83,38 @@ public class MultipleChoiceGame extends GameController {
     private void handleAnswer(int selectedAnswerIndex) {
         if (game != null && currentQuestion != null && currentAnswers != null) {
             String selectedAnswer = currentAnswers.get(selectedAnswerIndex);
-
+            selectAbtn.setDisable(true);
+            selectBbtn.setDisable(true);
+            selectCbtn.setDisable(true);
+            selectDbtn.setDisable(true);
             if (currentQuestion.checkAnswers(selectedAnswer)) {
                 correct.setVisible(true);
                 correct1.setVisible(true);
                 wrong.setVisible(false);
                 wrong1.setVisible(false);
+                game.increasePoint();
+                healthText.setText("Health: " + game.getHealth());
+                scoreText.setText("Score: " + game.getPoint());
+
             } else {
                 correct.setVisible(false);
                 correct1.setVisible(false);
                 wrong.setVisible(true);
                 wrong1.setVisible(true);
+                game.decreaseHealth();
+                healthText.setText("Health: " + game.getHealth());
+                scoreText.setText("Score: " + game.getPoint());
             }
-
+            if (game.getHealth() == 0) {
+                replayBtn.setDisable(false);
+                nextquestionbtn.setDisable(true);
+                contentQuestion.setText("Your score: " + game.getPoint() + "\nYou lose!");
+            }
+            if (game.getPoint() == 5) {
+                replayBtn.setDisable(false);
+                nextquestionbtn.setDisable(true);
+                contentQuestion.setText("Your score: " + game.getPoint() + "\nYou win!");
+            }
             // Kiểm tra và xử lý kết thúc trò chơi ở đây nếu cần
         }
     }
